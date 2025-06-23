e5472387020704d18f1b6be6d18e9cdf96447673
rnimisha <nimisaraut@gmail.com>
2025-06-23 13:08:15 +0545

    feat: add classifer decoder

--- model.py
+++ model.py
@@ -1,16 +1,33 @@
 import torch
 import torch.nn.functional as F
+from torch import Tensor
 from torch_geometric.nn import GCNConv
 
 
 class GNN(torch.nn.Module):
-    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int):
+    def __init__(self, hidden_channels: int):
         super().__init__()
 
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.conv1 = GCNConv(hidden_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
 
-    def forward(self, x, edge_index, edge_weight):
+    def forward(self, x: Tensor, edge_index: Tensor, edge_weight: Tensor):
         x = F.relu(self.conv1(x, edge_index, edge_weight))
         x = self.conv2(x, edge_index, edge_weight)
         return x
+
+
+class PairingClassifier(torch.nn.Module):
+    def forward(self, x_ing1: Tensor, x_ing2: Tensor, edge_label_index: Tensor):
+        edge_feat_ing1 = x_ing1[edge_label_index[0]]
+        edge_feat_ing2 = x_ing2[edge_label_index[1]]
+        return (edge_feat_ing1 * edge_feat_ing2).sum(dim=-1)
+
+
+class Model(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward():
+        pass
