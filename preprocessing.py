3930bf52da0ec97600e1249d4ad90ab1a02ba95b
rnimisha <nimisaraut@gmail.com>
2025-06-26 15:26:16 +0545

    feat: add compound based

--- preprocessing.py
+++ preprocessing.py
@@ -25,21 +25,36 @@ metapath_chemical = [
 model = PairingModel(data)
 
 # %%
-metapaths = [("ingredient", "paired_with", "ingredient")]
-recipe_model = model.create_model(metapaths, embedding_dim=128)
+recipe_model = model.create_model(metapath_recipe, embedding_dim=128)
+comp_ingr_model = model.create_model(metapath_chemical, embedding_dim=128)
 # %%
 
-save_path = os.path.join("model", "recipe_model.pt")
+save_path_recipe = os.path.join("model", "recipe_model.pt")
+save_path_substitution = os.path.join("model", "comp_ingr_model.pt")
 # %%
-model.train_model(recipe_model, save_path)
+model.train_model(recipe_model, save_path_recipe)
+
+# %%
+model.train_model(comp_ingr_model, save_path_substitution)
 
 # %%
 
-recipe_model.load_state_dict(torch.load(save_path))
-recommender = recipe_model
+recipe_model.load_state_dict(torch.load(save_path_recipe))
+recipe_recommender = recipe_model
 
 # %%
-a = recommend_pairs(recommender, "tomato", idx_to_name)
+a = recommend_pairs(recipe_recommender, "tomato", idx_to_name)
 # %%
 a
+# %%
+comp_ingr_model.load_state_dict(torch.load(save_path_substitution))
+subsitution_recommender = comp_ingr_model
+
+# %%
+a = recommend_pairs(comp_ingr_model, "onion", idx_to_name)
+# %%
+a
+# %%
+data
+
 # %%
